a219f1a7d4ed14c70d7eb2ec3b5c0e2dc5a1d0aa
Qwertana <najuprogramadora@gmail.com>
2026-02-28 15:18:05 -0300

    Create teste.vue

---
+++ teste.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+ <h1>Sua idade é:</h1>
+ <h2>{{idade}}</h2>
+ <input type="number" v-model.number="idade">
+ <br><br> 
+ <button @click="aumentarIdade">Aumentar idade</button>
+ <button @click="diminuirIdade">Diminuir idade</button>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            idade: 24
+        }
+    },
+    methods: {
+        aumentarIdade() {
+            this.idade++
+        },
+        diminuirIdade(){
+            this.idade--
+        }
+    }
+}
+</script>
